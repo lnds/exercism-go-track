01d41972326c7bcc6a52c696f23b6d0bd2c1d2a3
Eduardo Díaz <ediaz@dcc8090.cl>
2021-08-08 21:31:26 -0400

    add additional tests to robot name

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -3,6 +3,7 @@ package robotname
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 type Robot struct {
@@ -10,22 +11,41 @@ type Robot struct {
 }
 
 func (robot *Robot) Name() (string, error) {
-	if robot.name == "" {
+	if robot.name != "" {
+		return robot.name, nil
+	}
+
+	if len(names) >= MAX_NAMES {
+		return "", fmt.Errorf("no more names")
+	}
+	robot.name = randName()
+	for names[robot.name] {
 		robot.name = randName()
 	}
+	names[robot.name] = true
 	return robot.name, nil
 }
 
-func (robot *Robot) Reset() (string, error) {
-	robot.name = randName()
-	return robot.name, nil
+func (robot *Robot) Reset() {
+	robot.name = ""
 }
 
 var letters = [26]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 
+var names = map[string]bool{}
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const MAX_PREFIX = 26 * 26
+const MAX_SUFIX = 10 * 10 * 10
+const MAX_NAMES = MAX_PREFIX * MAX_SUFIX
+
 func randName() string {
-	rand.Shuffle(len(letters), func(i, j int) {
-		letters[i], letters[j] = letters[j], letters[i]
-	})
-	return fmt.Sprintf("%c%c%03d", letters[0], letters[1], rand.Int()%1000)
+	a := randUpperChar()
+	b := randUpperChar()
+	num := random.Intn(MAX_SUFIX)
+	return fmt.Sprintf("%c%c%03d", a, b, num)
+}
+
+func randUpperChar() int {
+	return 'A' + random.Intn(26)
 }
